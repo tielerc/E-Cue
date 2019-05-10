5ce7dd8e01d95d3132bb45274fe1669dd05afd4a
Tieler Callazo <tieler@stanford.edu>
2019-05-09 17:56:35 -0700

    Scenechanges.

---
+++ E-Cue/Assets/Scripts/ChangeSceneTest.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class ChangeSceneTest : MonoBehaviour
+{
+    // void OnEnable()
+    // {
+    //     // if(Input.GetKeyDown("y"))
+    //     // {
+    //     //     SceneManager.LoadScene("SceneTest_Y", LoadSceneMode.Single);
+    //     //     print("y key was pressed");
+    //     // } else if(Input.GetKeyDown("y"))
+    //     // {
+    //     //     SceneManager.LoadScene("SceneTest_N", LoadSceneMode.Single);
+    //     //     print("N key was pressed");
+    //     // }
+    // }
+
+    void Update()
+    {
+        if(Input.GetKeyDown("y"))
+        {
+            SceneManager.LoadScene("SceneTest_Y", LoadSceneMode.Single);
+            print("y key was pressed");
+        } else if(Input.GetKeyDown("n"))
+        {
+            SceneManager.LoadScene("SceneTest_N", LoadSceneMode.Single);
+            print("N key was pressed");
+        }
+    }
+}
